eafac46c4ca56d93ae078d83b8629816b83ec30e
JinHeng Li <jinheng2@illinois.edu>
2026-09-20 16:25:31 -0400

    (chore) add mockingData file

---
+++ frontend/src/Utils/mockingData.tsx
@@ -0,0 +1,74 @@
+export const MOCK_JOB_CARD_DATA = [
+            {
+                percentMatch : 64, 
+                jobTitle: "Web Application Developer", 
+                companyAvatarPath: "www.abcde.xyz", 
+                companyName: "Backd Business Funding", 
+                jobLocation: "Austin, Texas Matropolitan Area", 
+                workingMode: "On-site", 
+                jobTags: ["Full Time", "0 of 3 skills matched", "Mid-Level", "$65/yr-$70/yr"], 
+                timePosted: 1789325624000,
+                numOfApplicants: 25, 
+                jobLink: "https://www.bbf.com", 
+                isHearted: false
+            }, 
+            {
+                percentMatch : 93, 
+                jobTitle: "Software Engineering, Network Infrastructure", 
+                companyAvatarPath: "www.abcde.xyz", 
+                companyName: "Cursor AI", 
+                jobLocation: "Sunnyvale, CA", 
+                workingMode: "On-site", 
+                jobTags: ["Full Time", "5+ years exp", "Mid-Level", "$161k/yr-$239k/yr"], 
+                timePosted: 1789318624000,
+                numOfApplicants: 25, 
+                jobLink: "https://www.cursorai.com", 
+                isHearted: true
+            }, 
+            {
+                percentMatch : 82, 
+                jobTitle: "Full-Stack Software Engineer (Web Developer)", 
+                companyAvatarPath: "www.abcde.xyz", 
+                companyName: "Simons Foundation", 
+                jobLocation: "New York, NY", 
+                workingMode: "On-site", 
+                jobTags: ["Full Time", "5+ years exp", "Mid-Level", "$125k/yr-$140k/yr"], 
+                timePosted: 1789318624000,
+                numOfApplicants: 25, 
+                jobLink: "https://www.cursorai.com", 
+                isHearted: false
+            }, 
+            {
+                percentMatch : 82, 
+                jobTitle: "Full-Stack Software Engineer (Web Developer)", 
+                companyAvatarPath: "www.abcde.xyz", 
+                companyName: "Simons Foundation", 
+                jobLocation: "New York, NY", 
+                workingMode: "On-site", 
+                jobTags: ["Full Time", "5+ years exp", "Mid-Level", "$125k/yr-$140k/yr"], 
+                timePosted: 1789318624000,
+                numOfApplicants: 25, 
+                jobLink: "https://www.cursorai.com", 
+                isHearted: false
+            }, 
+            {
+                percentMatch : 82, 
+                jobTitle: "Full-Stack Software Engineer (Web Developer)", 
+                companyAvatarPath: "www.abcde.xyz", 
+                companyName: "Simons Foundation", 
+                jobLocation: "New York, NY", 
+                workingMode: "On-site", 
+                jobTags: ["Full Time", "5+ years exp", "Mid-Level", "$125k/yr-$140k/yr"], 
+                timePosted: 1789318624000,
+                numOfApplicants: 25, 
+                jobLink: "https://www.cursorai.com", 
+                isHearted: false
+            }
+        ]
+
+export const MOCK_JOB_DATA = [
+    {
+        jobLink: "https://www.bbf.com",
+        jobDescription: "Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description Job Description"
+    }
+]
